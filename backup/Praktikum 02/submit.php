8ec5cc58ac3b5fe594f20cd8340c6105f678377d
MuhamadSayyidFadhil <mhmmdsydfdhl@gmail.com>
2024-03-28 14:55:26 +0700

    hapus backup

--- backup/Praktikum 02/submit.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-echo "<h1>Selamat datang $username</h1>";
--- backup/Praktikum 03/looping2.php
+++
@@ -1,7 +0,0 @@
-<?php
-//foreach
-$buah = ['Mangga', 'Jeruk', 'Semanggka'];
-
-foreach ($buah as $b){
-    echo "<h1>{$b}</h1>";
-}
